e61561b5449bba6830a312a36ba42da54aad7045
Jose Luis Colcha <jose.colcha@epn.edu.ec>
2021-03-15 23:37:19 -0500

    Calendar Version 1

--- poli-news/src/pages/CalendarPage.js
+++ poli-news/src/pages/CalendarPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CalendarEverywhere from '../components/CalendarEverywhere';
 import Footer from '../components/Footer';
 import withAuth from '../hocs/withAuth';
 
@@ -8,6 +9,10 @@ const CalendarPage = () => {
 			<hr />
 			<h1 className="my-title">CALENDARIO</h1>
 			<hr />
+
+			<div style={{ margin: '0 10%' }}>
+				<CalendarEverywhere />
+			</div>
 			<Footer />
 		</>
 	);
